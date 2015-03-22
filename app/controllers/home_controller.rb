775b076f0705d6a9cc5200d64ab9554cd3ac246a
Jeffrey O Omiyi <5sp4lr@gmail.com>
2015-03-21 20:41:54 -0500

    Bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+  	@products = Shoppe::Product.root.ordered.includes(:product_category, :variants)
+    @products = @products.group_by(&:product_category)
   end
 end
